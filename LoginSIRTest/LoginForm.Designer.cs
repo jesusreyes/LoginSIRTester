fab9fb9b4c64e2ba42ec65a6b1b3ea38d204052f
Jesus Reyes <jesusreyes@users.noreply.github.com>
2021-02-09 15:06:22 -0700

    First complete commit

---
+++ LoginSIRTest/LoginForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace LoginSIRTest
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtxtResponse = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUser.Location = new System.Drawing.Point(65, 41);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(277, 29);
+            this.txtUser.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(65, 112);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(277, 29);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(61, 18);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(64, 20);
+            this.lblUser.TabIndex = 2;
+            this.lblUser.Text = "Usuario";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(157, 161);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(99, 35);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Enviar";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password";
+            // 
+            // rtxtResponse
+            // 
+            this.rtxtResponse.Location = new System.Drawing.Point(48, 224);
+            this.rtxtResponse.Name = "rtxtResponse";
+            this.rtxtResponse.Size = new System.Drawing.Size(323, 140);
+            this.rtxtResponse.TabIndex = 6;
+            this.rtxtResponse.Text = "";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 387);
+            this.Controls.Add(this.rtxtResponse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(421, 426);
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        protected System.Windows.Forms.TextBox txtUser;
+        protected System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.RichTextBox rtxtResponse;
+    }
+}
+
